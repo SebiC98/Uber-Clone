0fe94eb7cdf42c39d25231b16d7ed7fba4a64d4f
SebiC98 <sebastiancioata98@gmail.com>
2023-01-17 11:54:59 +0200

    Base project

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Sebastian Cioată on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
